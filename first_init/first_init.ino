ed16c036a1f439ad68954cd30f966f360120b01e
Tobias Hintz <tobias.hintz@student.kit.edu>
2023-11-21 10:05:39 +0000

    lecture code

--- first_init/first_init.ino
+++ first_init/first_init.ino
@@ -3,6 +3,7 @@ int delayTime = 200;
 int clock = 1;
 int signal = 0;
 int wait = 20;
+byte data[2] = {0};
 
 
 void setup() {
@@ -10,7 +11,11 @@ void setup() {
   pinMode(0, OUTPUT); // D0
   pinMode(1, OUTPUT); // D1
 
+  pinMode(LED_BUILTIN, OUTPUT);
+
   Wire.begin(); // join I2C bus as the master; no adress given, not necessary for master
+
+  Serial.begin(9600);
 
   // configuring the 3 IO Expanders -> for now all pins to output -> easier (later set GPA0 & GPA1 to input for buttons)
   /*
@@ -25,29 +30,29 @@ void setup() {
   */
   Wire.beginTransmission(0x24);
   Wire.write(0x00); // IODIRA register
-  Wire.write(0x00); // set all of port A to outputs (0 = Output)
+  Wire.write(0x03); // set all of port A to outputs (0 = Output) 0000 0011
   Wire.endTransmission();
   Wire.beginTransmission(0x24);
   Wire.write(0x01); // IODIRB register
-  Wire.write(0x00); // set all of port B to outputs
+  Wire.write(0xC0); // set all of port B to outputs
   Wire.endTransmission();
 
   Wire.beginTransmission(0x26);
   Wire.write(0x00); // IODIRA register
-  Wire.write(0x00); // set all of port A to outputs
+  Wire.write(0x03); // set all of port A to outputs
   Wire.endTransmission();
   Wire.beginTransmission(0x26);
   Wire.write(0x01); // IODIRB register
-  Wire.write(0x00); // set all of port B to outputs
+  Wire.write(0xC0); // set all of port B to outputs
   Wire.endTransmission();
 
   Wire.beginTransmission(0x27);
   Wire.write(0x00); // IODIRA register
-  Wire.write(0x00); // set all of port A to outputs
+  Wire.write(0x03); // set all of port A to outputs
   Wire.endTransmission();
   Wire.beginTransmission(0x27);
   Wire.write(0x01); // IODIRB register
-  Wire.write(0x00); // set all of port B to outputs
+  Wire.write(0xC0); // set all of port B to outputs
   Wire.endTransmission();
 
 
@@ -76,7 +81,7 @@ void setup() {
   Wire.endTransmission();
   Wire.beginTransmission(0x27);
   Wire.write(0x13); 
-  Wire.write(0xFE); 
+  Wire.write(0xFF); 
   Wire.endTransmission();
 
 }
@@ -86,22 +91,57 @@ void loop() {
   // data inputs are logicialls ANDed, A is set to high in hardware -> we set input with B (Pin D0 of adruino)
   // CLK signal to push bits in register (Pin D1 of adruino)
 
-  // CLK signal
-  digitalWrite(clock, HIGH);
-  delay(delayTime);
-  digitalWrite(clock, LOW);
-  digitalWrite(signal, HIGH);
-  delay(delayTime);
-  digitalWrite(clock, HIGH);
-  delay(delayTime);
-  digitalWrite(clock, LOW);
-  //sigitalWrite(signal, LOW);
-  for (int i = 0; i < wait; i++){
-    digitalWrite(clock, HIGH);
-    delay(delayTime);
-    digitalWrite(clock, LOW);
-    delay(delayTime);
+  //Wire.beginTransmission(0x24);
+
+  Wire.requestFrom(0x24, 2);
+
+  // Check if data is available
+  if (Wire.available() >= 2) {
+    // Read data from the I2C device
+    for (int i = 0; i < 2; i++) {
+      data[i] = Wire.read();
+    }
   }
+
+
+  if(data[0] == 1){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else{
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  // Print the received data to the Serial monitor
+  Serial.print("Received data: ");
+  for (int i = 0; i < 2; i++) {
+    Serial.print(data[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+  
+
+
+
+
+
+
+
+  // CLK signal
+  // digitalWrite(clock, HIGH);
+  // delay(delayTime);
+  // digitalWrite(clock, LOW);
+  // digitalWrite(signal, HIGH);
+  // delay(delayTime);
+  // digitalWrite(clock, HIGH);
+  // delay(delayTime);
+  // digitalWrite(clock, LOW);
+  // //sigitalWrite(signal, LOW);
+  // for (int i = 0; i < wait; i++){
+  //   digitalWrite(clock, HIGH);
+  //   delay(delayTime);
+  //   digitalWrite(clock, LOW);
+  //   delay(delayTime);
+  // }
 
   // Wire.beginTransmission(0x27);
   // Wire.write(0x13); // GPIO A register
